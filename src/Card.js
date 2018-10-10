f1f47d6a5931205143410a828edfba6bb423f0e7
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-10-10 13:27:09 +0200

    Roboshit creation

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <h1>Robo Friends</h1>
+      <img src="https://robohash.org/test?200x200" alt="feafea"/>
+      <div>
+        <h2>Jane Doe</h2>
+        <p>Jane Doe gmail</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card
