b64b8b598a2d635a599ed2714f50f7d28c691153
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-10-18 12:31:35 +0200

    Part 2.3 done

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = () => {
+  return (
+    <div className='pa2'>
+    <input
+      className='pa3 ba b-red bg-lightest-blue'
+      type="search"
+      placeholder='Search Your robot'/>
+    </div>
+  );
+}
+
+export default SearchBox
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons'
 import './index.css';
-import CardList from './CardList'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CardList /> , document.getElementById('root'));
+ReactDOM.render(<App /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
