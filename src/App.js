8f5a4177297b5fa8775ed2d048de56d2a382f431
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-10-18 16:27:29 +0200

    Only website used for robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox'
+// import {robots} from './robots'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}))
+
   }
 
   onSearchChange = (event) => {
@@ -19,13 +26,17 @@ class App extends Component {
 
   render() {
     const filteredRobots =  this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()))
-    return  (
-      <div className='tc'>
-        <h1 className='f2'>Robot App With React</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if(this.state.robots.length === 0) {
+      return <h1 className='tc'>Loading</h1>
+    } else {
+      return  (
+        <div className='tc'>
+          <h1 className='f2'>Robot App With React</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
