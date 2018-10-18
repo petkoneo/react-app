4e9f1d1f4e1dee8742cb3f0ecef69683d67f4fa5
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-10-18 13:18:56 +0200

    version 3 done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox'
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>Robot App With React</h1>
-      <SearchBox/>
-    <CardList robots={robots} />
-    </div>
-  );
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const filteredRobots =  this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()))
+    return  (
+      <div className='tc'>
+        <h1>Robot App With React</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, searchChange}) => {
   return (
     <div className='pa2'>
     <input
       className='pa3 ba b-red bg-lightest-blue'
       type="search"
-      placeholder='Search Your robot'/>
+      placeholder='Search Your robot'
+    onChange={searchChange}/>
     </div>
   );
 }
