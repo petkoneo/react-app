afc0bd91de9b1b7b4e0b8fc4f17adbc5b201b8ff
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-10-18 16:38:17 +0200

    5th part done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from './CardList';
 import SearchBox from './SearchBox'
 // import {robots} from './robots'
 import './App.css'
+import Scroll from './Scroll'
 
 class App extends Component {
   constructor() {
@@ -33,7 +34,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f2'>Robot App With React</h1>
           <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
           <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       );
     }
