e5fadfea7de6755ce24ae0af9ef60f3692641aa0
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-10-31 12:48:24 +0100

    Some new stuff added new png added. PWA stuff implemented.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-3 tc'>
-      <img src={`https://robohash.org/${id}?200x200`} alt="feafea"/>
+      <img src={`https://robohash.org/${id}?size=300x300`} alt="feafea"/>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
