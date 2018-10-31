36722ad90deab59927dc703d4e398444cc4b6a31
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-10-31 12:17:48 +0100

    Support for github Pages added

---
+++ build/precache-manifest.34416c8a7bb68a72d23d05721d433e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-app-react/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "16f02a7a80b37bac0e46",
+    "url": "/robo-app-react/static/js/runtime~main.16f02a7a.js"
+  },
+  {
+    "revision": "d48f7dcf9ef14524b17e",
+    "url": "/robo-app-react/static/js/main.d48f7dcf.chunk.js"
+  },
+  {
+    "revision": "147628a0f12497e82022",
+    "url": "/robo-app-react/static/js/1.147628a0.chunk.js"
+  },
+  {
+    "revision": "d48f7dcf9ef14524b17e",
+    "url": "/robo-app-react/static/css/main.7cd28005.chunk.css"
+  },
+  {
+    "revision": "147628a0f12497e82022",
+    "url": "/robo-app-react/static/css/1.4be46b0f.chunk.css"
+  },
+  {
+    "revision": "7cc175706a35eb6ba5e094b685118de8",
+    "url": "/robo-app-react/index.html"
+  }
+];
