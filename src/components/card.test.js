c676b92d3e5515642c1b1a8cdbc4866a7c68c1d0
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-11-06 12:40:08 +0100

    Snapshot testing of two components done

--- src/components/card.test.js
+++ src/components/card.test.js
@@ -2,7 +2,9 @@ import { shallow } from 'enzyme';
 import React from 'react'
 import Card from './Card';
 
-it('expects to render the Card component',() => {
-    expect(shallow(<Card />).length).toEqual(1)
+
+it('Snapshot test of Card element', () => {
+    expect(shallow(<Card />)).toMatchSnapshot()
   }
 )
+
---
+++ src/components/cardlist.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import React from 'react'
+import CardList from './CardList';
+
+
+it('Snapshot test of CardList element', () => {
+  const mockRobots = [{
+    id:1,
+    name:"Michal",
+    username:"michels",
+    email:"Mich@el.com"
+  }]
+    expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot()
+  }
+)
+
